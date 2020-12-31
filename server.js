7539d852eab82f6dd4d38106fa1f54182374b807
OrangeDev2 <sabym777@gmail.com>
2020-12-31 15:43:11 -0800

    Added Procfile

--- server.js
+++ server.js
@@ -62,7 +62,11 @@ app.use(express.static(path.join(__dirname, 'build')));
 
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'), function(err){
+    if (err) {
+        res.status(500).send(err)
+      }
+  });
 });
 
 app.listen(PORT, function(){
