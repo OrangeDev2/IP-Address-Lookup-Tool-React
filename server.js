d94ad4236b3a7b646ebecab97762881d676a1999
OrangeDev2 <sabym777@gmail.com>
2020-12-31 17:22:31 -0800

    server.js changed /* to *

--- server.js
+++ server.js
@@ -60,8 +60,12 @@ app.get('/geolocation/:ip?', (req, res) => {
 app.use(express.static(path.join(__dirname, 'build')));
 
 
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './build/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  });
 });
 
 var port = process.env.PORT || 5000;
