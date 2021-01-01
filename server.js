a66d15496d123be1e121dcc28c2a459b9b22b81f
OrangeDev2 <sabym777@gmail.com>
2020-12-31 17:11:32 -0800

    Added headers

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(function(req, res, next) {
-    //res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', 'https://limitless-sierra-05152.herokuapp.com/');
     res.header(
       'Access-Control-Allow-Headers',
       'Origin, X-Requested-With, Content-Type, Accept'
