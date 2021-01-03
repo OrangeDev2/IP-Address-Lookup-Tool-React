c28d58a93c4bf4b897073faa887c39ba6013927e
OrangeDev2 <sabym777@gmail.com>
2021-01-02 19:44:51 -0800

    Moved redirect to above routes

--- server.js
+++ server.js
@@ -21,6 +21,20 @@ app.use(function(req, res, next) {
 app.enable('trust proxy');
 app.set('trust proxy', true);
 
+app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+app.get('*', (req, res) => {
+  if (req.headers['x-forwarded-proto'] != 'https' && process.env.NODE_ENV === "production") {
+    res.redirect('https://showip.io' + req.url);
+  }
+  else {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'), function(err) {
+      if (err) {
+        res.status(500).send(err)
+      }
+    });
+  }
+});
 
 app.get('/geolocation/:ip?', (req, res) => {
     //    if (req.params.ip.match(/^(?:(?:2[0-4]\d|25[0-5]|1\d{2}|[1-9]?\d)\.){3}(?:2[0-4]\d|25[0-5]|1\d{2}|[1-9]?\d)(?:\:(?:\d|[1-9]\d{1,3}|[1-5]\d{4}|6[0-4]\d{3}|65[0-4]\d{2}|655[0-2]\d|6553[0-5]))?$/) || req.params.ip.match(/^(?:^|(?<=\s))(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))(?=\s|$)/)){
@@ -53,21 +67,6 @@ app.get('/geolocation/:ip?', (req, res) => {
         });
     }
 
-});
-
-app.use(express.static(path.join(__dirname, 'client', 'build')));
-
-app.get('*', (req, res) => {
-  if (req.headers['x-forwarded-proto'] != 'https' && process.env.NODE_ENV === "production") {
-    res.redirect('https://showip.io' + req.url);
-  }
-  else {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'), function(err) {
-      if (err) {
-        res.status(500).send(err)
-      }
-    });
-  }
 });
 
 var port = process.env.PORT || 8000;
