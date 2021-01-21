23f4c139094638157b11c5f40e7d5948a36ea165
OrangeDev2 <47803678+OrangeDev2@users.noreply.github.com>
2021-01-20 20:32:03 -0800

    Removed css; Added banner/pictures

--- client/src/pages/about.jsx
+++ client/src/pages/about.jsx
@@ -5,7 +5,10 @@ class AboutPage extends Component {
         return (
             <div style={{marginTop: "20vh"}}>
                 <h3>ShowIP.io is a free service that shows your public IP Address and Geolocation with Google Maps API! <br></br>IP Address Lookup (IPv4, IPv6) is also available <a href="/ip-search">here</a>.</h3>
-                <p style={{marginTop: "10vh"}}>If you have any questions. Please do not hesitate to contact the owner.</p>
+                <a href="https://nordvpn.com/special/?utm_medium=affiliate&utm_term&utm_content&utm_campaign=off15&utm_source=aff50645" target="_blank" rel="noreferrer">
+                    <img src="./../nordvpn-assests/changeIP.jpg" style={{height: "50vh"}}></img>
+                        </a>
+                <p>If you have any questions. Please do not hesitate to contact the owner.</p>
                 <p>Email: <a href="mailto:admin@showip.io" target="_blank" rel="noreferrer" style={{color: "grey"}}>admin@showip.io</a></p>
             </div>
         )
